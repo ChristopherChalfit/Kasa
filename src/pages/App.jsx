198ce7713f2c23b358906eb336e0356f50db9cb5
Christopher Chalafit <christopher.chalafit@hotmail.com>
2023-10-06 18:04:21 +0200

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -16,10 +16,10 @@ function App() {
     <BrowserRouter >
       <Header />
       <Routes>
-        <Route path="/Kasa" element={<Home logements={logement} />} />
-        <Route path="/Kasa/about" element={<About />} />
+        <Route path="/" element={<Home logements={logement} />} />
+        <Route path="/about" element={<About />} />
         <Route
-          path="/Kasa/logements/:id"
+          path="/logements/:id"
           element={<Logements logement={logement} />}
         />
 
